25b7e8fea180c853dc861c6efba733dd2da8186c
Melecio Paes III <vandheerlorde309@yahoo.com>
2024-12-19 14:14:43 +0800

    Add Service Worker and manifest.json for PWA support

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'cat-fact-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html', 
+    '/script.js',  
+    '/manifest.json',
+    '/icon-192x192.png',
+    '/icon-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) =>
